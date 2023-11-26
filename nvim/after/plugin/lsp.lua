242281f4576482fdf8cf67f3cf604bb74914f756
Julien Hallé <julienhalle@gmail.com>
2023-11-26 07:27:18 -0500

    lsp configuration

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,10 +1,18 @@
--- require("mason").setup()
 
-
--- local lsp = require('lsp-zero')
-
--- lsp.preset('recommended')
--- lsp.setup()
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig').gopls.setup{
+    capabilities = capabilities,
+    on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer = 0})
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
+        vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer = 0})
+        vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer = 0})
+        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer = 0})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer = 0})
+    end,
+}
 
 
 -- require("mason-lspconfig").setup  {
@@ -20,4 +28,5 @@
 -- })
 -- end,
 -- }
+--jq
 -- }
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,7 +44,10 @@ require('lazy').setup({
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	{'neovim/nvim-lspconfig'},
 	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/cmp-buffer'},
+	{'hrsh7th/cmp-path'},
 	{'hrsh7th/nvim-cmp'},
+	{'saadparwaiz1/cmp_luasnip'},
 	{'L3MON4D3/LuaSnip'},
 })
 
